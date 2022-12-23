431f99ced7d2f9f2e256681cc6d2acf6a4dbe1a7
PraiseXI <praisexi@outlook.com>
2022-12-23 17:36:40 +0000

    add logic for buttons, change joke to normal jokes

--- script.js
+++ script.js
@@ -3,17 +3,33 @@ async function fetchJoke() {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'b70b3eebe7mshb6ccde910a70443p1413b6jsnaa296c327351',
-            'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+            'X-RapidAPI-Host': 'joke110.p.rapidapi.com'
         }
     };
-    
-    const resp = await fetch ('https://dad-jokes.p.rapidapi.com/random/joke', options)
+
+    const resp = await fetch('https://joke110.p.rapidapi.com/random_joke', options)
     const record = await resp.json()
 
-    document.getElementById("setup").innerHTML = record.body[0].setup
-    document.getElementById("punchline").innerHTML = record.body[0].punchline
+    document.getElementById("setup").innerHTML = record.setup
+    document.getElementById("punchline").innerHTML = record.punchline
+
+    const revealButton = document.getElementById("button");
+    const refreshButton = document.getElementById("refresh");
+    const punchLine = document.getElementById("punchline");
+    //shows punchline
+    button.addEventListener('click', function () {
+        punchLine.style.display = 'block';
+    });
+    //hides reveal button
+    revealButton.addEventListener('click', function () {
+        revealButton.style.display = 'none';
+    });
+    //event click for refresh button
+    refreshButton.addEventListener('click', function () {
+        window.location.reload();
+    });
+
 }
 fetchJoke();
-//TODO: CREATE BUTTON TO REVEAL PUNCHLINE
-//TODO: CREATE BUTTON TO CREATE NEW JOKE
+
 //TODO: IMPROVE VISUAL
